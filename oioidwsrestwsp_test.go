1c38827790552f93a5b7aefc39e24fc2846325e9
evtr <evtr@kvalitetsit.dk>
2019-11-21 12:29:39 +0100

    test for hok that fails

--- oioidwsrestwsp_test.go
+++ oioidwsrestwsp_test.go
@@ -18,7 +18,7 @@ func TestCallWspServerWithoutClientSSLCertifikatFails(t *testing.T) {
 
 	// Given
 	config := createConfig()
-	httpServer := createOioIdwsWsp(config, nil, nil)
+	httpServer, _ := createOioIdwsWsp(config, nil, nil)
 	httpClient := httpServer.Client()
 
 	// When
@@ -33,7 +33,7 @@ func TestCallWspWithClientSSLCertificateButWithoutAuthenticationTokenFails(t *te
 
         // Given
         config := createConfig()
-        httpServer := createOioIdwsWsp(config, nil, mockClientCertificate)
+        httpServer, _ := createOioIdwsWsp(config, nil, mockClientCertificate)
 	httpClient := httpServer.Client()
 
         // When
@@ -43,12 +43,42 @@ func TestCallWspWithClientSSLCertificateButWithoutAuthenticationTokenFails(t *te
         assert.Equal(t, http.StatusUnauthorized, res.StatusCode)
 }
 
+func TestThatAuthenticatedSessionCannotBeHijackedBecauseOfHoKValidation(t *testing.T) {
+        // Given
+        config := createConfig()
+        httpServer, wsp := createOioIdwsWsp(config, createMongoSessionCache(), mockClientCertificate)
+        httpClient := httpServer.Client()
+        wsc, _ := CreateTestOioIdwsRestHttpProtocolClient()
+        encodedToken, _ := wsc.GetEncodedTokenFromSts([]byte{}, nil)
+        authRequest := fmt.Sprintf("saml-token=%s", encodedToken)
+        authUrl := fmt.Sprintf("%s/token", httpServer.URL)
+        serviceUrl := fmt.Sprintf("%s/hello", httpServer.URL)
+
+        // When
+        authResp, authErr := httpClient.Post(authUrl, "any", strings.NewReader(authRequest))
+        oioIdwsRestAuth, authParseErr := CreateOioIdwsRestAuthResponseFromHttpReponse(authResp)
+	wsp.ClientCertHandler = mockHijackerCertificate
+        serviceRequest := createServiceRequest(serviceUrl, oioIdwsRestAuth)
+        serviceResp, serviceErr := httpClient.Do(serviceRequest)
+
+        // Then
+        assert.NilError(t, authErr)
+        assert.NilError(t, authParseErr)
+        assert.NilError(t, serviceErr)
+
+        assert.Equal(t, http.StatusOK, authResp.StatusCode)
+        assert.Equal(t, "Holder-of-key", oioIdwsRestAuth.TokenType)
+        assert.Assert(t, oioIdwsRestAuth.ExpiresIn > 0)
+        assert.Assert(t, len(oioIdwsRestAuth.AccessToken) > 0)
+
+        assert.Equal(t, http.StatusUnauthorized, serviceResp.StatusCode)
+}
 
 func TestAuthenticateAndCallingServiceUsingLegalTokenAndClientCertificate(t *testing.T) {
 
 	// Given
         config := createConfig()
-        httpServer := createOioIdwsWsp(config, createMongoSessionCache(), mockClientCertificate)
+        httpServer, _ := createOioIdwsWsp(config, createMongoSessionCache(), mockClientCertificate)
         httpClient := httpServer.Client()
 	wsc, _ := CreateTestOioIdwsRestHttpProtocolClient()
 	encodedToken, _ := wsc.GetEncodedTokenFromSts([]byte{}, nil)
@@ -112,6 +142,11 @@ func mockClientCertificate(req *http.Request) *x509.Certificate  {
 	return readCertificate("./testdata/medcom.cer")
 }
 
+func mockHijackerCertificate(req *http.Request) *x509.Certificate  {
+        return readCertificate("./testdata/other.cer")
+}
+
+
 func readCertificate(filename string) *x509.Certificate  {
         cert, _ := ioutil.ReadFile(filename)
         certBlock, _ := pem.Decode([]byte(cert))
@@ -125,7 +160,7 @@ func createMongoSessionCache() securityprotocol.SessionCache {
 	return res
 }
 
-func createOioIdwsWsp(config *OioIdwsRestHttpProtocolServerConfig, sessionCache securityprotocol.SessionCache, clientCertHandler func(*http.Request) *x509.Certificate) *httptest.Server {
+func createOioIdwsWsp(config *OioIdwsRestHttpProtocolServerConfig, sessionCache securityprotocol.SessionCache, clientCertHandler func(*http.Request) *x509.Certificate) (*httptest.Server, *OioIdwsRestWsp) {
 
 	wsp := NewOioIdwsRestWspFromConfig(config, sessionCache)
 	if (clientCertHandler != nil) {
@@ -141,7 +176,7 @@ func createOioIdwsWsp(config *OioIdwsRestHttpProtocolServerConfig, sessionCache 
 		}
 	}
 
-	return createTlsServer(handler)
+	return createTlsServer(handler), wsp
 }
 
 
